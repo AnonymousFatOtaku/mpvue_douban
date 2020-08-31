0a9f20c405fa07859cd972fad5f007b11a706e8c
damu <damu@example.com>
2020-08-31 17:51:50 +0800

    图书列表页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,74 @@
 <template>
   <div id="indexContainer">
-    index组件
+    <swiper indicator-dots indicator-color="pink" indicator-active-color="green" >
+      <swiper-item v-for="(item, index) in carouel" :key="index">
+        <img :src="item.image" alt="">
+      </swiper-item>
+    </swiper>
+    <div class="booksList">
+      <div class="listNav">
+        <span>全部商品</span>
+        <span class="more"> > </span>
+      </div>
+      <div class="list">
+        <div class="bookItem" v-for="(item,index) in booksArr" :key="index">
+          <img :src="item.image" alt="">
+          <p>《{{item.title}}》</p>
+          <p>{{item.author}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import datas from './datas/data.json'
+  console.log(datas, typeof datas);
+  export default {
+    data(){
+      return {
+        booksArr: datas
+      }
+    },
+    computed: {
+      carouel(){
+        // splice方法会修改原数组
+        return [...this.booksArr].splice(0, 4)
+      }
+    },
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  #indexContainer
+    swiper
+      width 100%
+      height 400rpx
+      img
+        width 100%
+        height 100%
+    .booksList
+      .listNav
+        padding 10rpx
+        .more
+          float right
+      .list
+        display flex
+        flex-wrap wrap
+        .bookItem
+          width 50%
+          display flex
+          flex-direction column
+          align-items center
+          border-bottom 1rpx solid #eee
+          box-sizing border-box
+          &:nth-child(2n + 1)
+            border-right 1rpx solid #eee
+          img
+            width 200rpx
+            height 200rpx
+            margin 10rpx 0
+          p
+            font-size 24rpx
+            line-height 50rpx
 </style>
